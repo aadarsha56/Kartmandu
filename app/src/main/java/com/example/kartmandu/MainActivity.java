e7a995186f9421f8f788f247d0079c4ba2727284
aadarsha56 <aado1456@gmail.com>
2019-07-09 22:07:28 +0545

    tablayout login

--- app/src/main/java/com/example/kartmandu/MainActivity.java
+++ app/src/main/java/com/example/kartmandu/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.kartmandu;
 
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.kartmandu.Adapter.ViewPageAdapter;
+import com.example.kartmandu.Fragments.LoginFragment;
+import com.example.kartmandu.Fragments.RegisterFragment;
+
 public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout=findViewById(R.id.tabLayout);
+        viewPager=findViewById(R.id.viewPager);
+
+        ViewPageAdapter adapter=new ViewPageAdapter(getSupportFragmentManager());
+
+
+        adapter.addFragment(new LoginFragment(),"Login");
+        adapter.addFragment(new RegisterFragment(),"Register");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
